e55f800a8b49b6f0675b309ac3aa4b599ec87c89
Ianara Nicole Dionisio <ianaranicoledionisio@gmail.com>
2025-10-28 08:34:32 +0800

    Add Division functionality to the calculator

--- calculator.js
+++ calculator.js
@@ -10,9 +10,14 @@ function multiply(a, b){
     return a*b
 }
 
+function divide(a, b){
+    return a/b
+}
+
 let a = 6
 let b = 3
 
 console.log("ADD: ", add(a, b))
 console.log("SUBTRACT: ", subtract(a, b))
 console.log("MULTIPLY: ", multiply(a, b))
+console.log("DIVIDE: ", divide(a, b))
